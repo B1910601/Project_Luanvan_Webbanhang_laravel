73e52960f03f48d061515de0d4b4ba34e241dc23
vib1910601 <vib1910601@student.ctu.edu.vn>
2023-05-29 19:36:32 +0700

    Hien thi san pham theo danh muc va thuong hieu

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ use App\Http\Controllers\ProductController;
 //fornt end
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/trang-chu', [HomeController::class, 'index']);
+//danh muc san pham trang chu
+Route::get('/danh-muc-san-pham/{category_id}', [CategoryProduct::class, 'show_category_home']);
+Route::get('/thuong-hieu-san-pham/{brand_id}', [BrandProduct::class, 'show_brand_home']);
 
 
 //backend
